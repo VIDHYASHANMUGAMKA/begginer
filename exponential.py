6cb67438dbd4c57bfafd3508b863fea19e2c8483
VIDHYASHANMUGAMKA <37236109+VIDHYASHANMUGAMKA@users.noreply.github.com>
2019-02-05 11:30:21 +0530

    Update exponential.py

--- exponential.py
+++ exponential.py
@@ -7,7 +7,5 @@ def power(base,exp):
         return(math.pow(base, power))
         return(base*power(base,exp-1))
 base=int(input("Enter base: "))
-exp=int(input("Enter exponential value: "))
 power=int(input("Enter the power value:"))
-print("power value:",math.pow(base, power))
-print("exponential value:",math.exp( base ))
+print("power value:",math.pow(base, power)) 
